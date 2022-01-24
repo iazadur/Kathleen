932ab96f66b919f2abb6fc9d270851d5e22eff68
iazadur <iamazadur@gmail.com>
2022-01-24 12:36:56 +0600

    complete product details section

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BiUser } from 'react-icons/bi'
 
 const ProductDetails = () => {
+    const [quantity, setQuantity] = useState(1)
     return (
         <>
             <section class="text-gray-600 body-font overflow-hidden">
                 <div class="container px-5 py-24 mx-auto">
                     <div class="lg:w-4/5 mx-auto flex flex-wrap ">
-                        <div className=" flex gap-x-2 sm:flex-col lg:flex-row sm:w-full lg:w-1/2 ">
-                            <div className="flex lg:flex-col sm:flex-row lg:w-2/12 sm:w-full gap-y-4 sm:order-1 lg:order-0 sm:gap-2 sm:my-3">
-                                <img alt="ecommerce" class="lg:w-full sm:w-1/4 sm:h-auto lg:h-auto h-64 object-cover object-center rounded" src="https://dummyimage.com/100x100" />
-                                <img alt="ecommerce" class="lg:w-full sm:w-1/4 sm:h-auto lg:h-auto h-64 object-cover object-center rounded" src="https://dummyimage.com/100x100" />
-                                <img alt="ecommerce" class="lg:w-full sm:w-1/4 sm:h-auto lg:h-auto h-64 object-cover object-center rounded" src="https://dummyimage.com/100x100" />
-                                <img alt="ecommerce" class="lg:w-full sm:w-1/4 sm:h-auto lg:h-auto h-64 object-cover object-center rounded" src="https://dummyimage.com/100x100" />
+                        <div className=" flex gap-x-2 flex-col lg:flex-row w-full lg:w-1/2 ">
+                            <div className="flex lg:flex-col flex-row lg:w-2/12 w-full gap-y-4 order-1 lg:order-0 gap-2 my-3">
+                                <img alt="ecommerce" class="lg:w-full w-1/4 h-auto lg:h-auto object-cover object-center rounded" src="https://dummyimage.com/100x100" />
+                                <img alt="ecommerce" class="lg:w-full w-1/4 h-auto lg:h-auto object-cover object-center rounded" src="https://dummyimage.com/100x100" />
+                                <img alt="ecommerce" class="lg:w-full w-1/4 h-auto lg:h-auto object-cover object-center rounded" src="https://dummyimage.com/100x100" />
+                                <img alt="ecommerce" class="lg:w-full w-1/4 h-auto lg:h-auto object-cover object-center rounded" src="https://dummyimage.com/100x100" />
                             </div>
-                            <img alt="ecommerce" class="sm:order-0 lg:order-1 w-full sm:h-auto lg:h-auto lg:w-10/12  h-64 object-cover object-center rounded" src="https://dummyimage.com/400x400" />
+                            <img alt="ecommerce" class="order-0 lg:order-1 w-full h-auto lg:h-auto lg:w-10/12  object-cover object-center rounded" src="https://dummyimage.com/400x400" />
                         </div>
                         <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0 ">
                             <h1 class="text-gray-900 text-5xl title-font font-medium mb-1">Kathleen Mud Mask</h1>
@@ -29,11 +30,11 @@ const ProductDetails = () => {
                             </p>
                             <div className="flex justify-between">
                                 <div className="flex">
-                                    <button className='border py-1 px-4 text-4xl border-black'>-</button>
-                                    <button className='border py-1 px-5 text-xl border-x-0  border-black'>1</button>
-                                    <button className='border py-1 px-4 text-2xl border-black'>+</button>
+                                    <button className='border md:py-1 md:px-4 py-0 px-2 text-4xl border-black' onClick={() => setQuantity(quantity && quantity - 1)}>-</button>
+                                    <button className='border md:py-1 md:px-5 py-0 px-2 text-xl border-x-0  border-black'>{quantity}</button>
+                                    <button className='border md:py-1 md:px-4 py-0 px-2 text-2xl border-black' onClick={() => setQuantity(quantity + 1)}>+</button>
                                 </div>
-                                <button className='font-semibold text24 text-white rounded-full py-2 px-16' style={{ backgroundColor: "#C1A544" }}>Add to cart</button>
+                                <button className='font-semibold text24 text-xl text-white rounded-full lg:py-2 lg:px-16 py-1 px-6' style={{ backgroundColor: "#C1A544" }}>Add to cart</button>
                             </div>
                             <div className="grid grid-cols-2 my-4 gap-y-3">
                                 {
