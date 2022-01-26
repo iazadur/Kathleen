b1650bc74987c7285d61e0eab85f280c9465e3de
iazadur <iamazadur@gmail.com>
2022-01-26 16:29:15 +0600

    some error fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
                     <h1 className='text-8xl font-extrabold'>Kathlen</h1>
                     <h3 className='text-8xl font-bold'>Mineral Mud Mask</h3>
                     <p className='text-xs my-5 w-2/3 break-all'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ipsum eligendi perspiciatis doloribus qui adipisci. Modi, sit officiis! Placeat nostrum expedita odio at tempora repellat nemo molestias hic, aspernatur, provident exercitationem corporis assumenda.</p>
-                    <button className='uppercase font-semibold text-xl border-2 border-amber-600 rounded-3xl px-2 py-1'>Shop now</button>
+                    <button className='uppercase font-semibold text-xl border  rounded-3xl px-12 py-2' style={{ borderColor:"#4F4002"}}>Shop now</button>
                 </div>
             </div>
         </div>
--- src/components/Shared/TextCard.jsx
+++ src/components/Shared/TextCard.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const TextCard = ({ heading, paragraph }) => {
     return (
         <div class="flex flex-col text-center w-full my-20">
-            <h1 class="sm:text-3xl text-2xl font-bold title-font mb-4 text-gray-900">{heading}</h1>
-            <p class="lg:w-2/3 mx-auto leading-relaxed font-normal text-xl">{paragraph}</p>
+            <h1 class="sm:text-3xl text-2xl font-bold title-font mb-4" style={{ color:"#882643"}}>{heading}</h1>
+            <p class="lg:w-2/3 mx-auto leading-relaxed font-normal text-xl text-black">{paragraph}</p>
         </div>
     );
 };
