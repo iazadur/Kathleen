affa963c743e7861bc9bfbd071ffd06b6eaebc33
iazadur <iamazadur@gmail.com>
2022-01-26 17:35:02 +0600

    some error fixed

--- src/components/CustomerSay.jsx
+++ src/components/CustomerSay.jsx
@@ -5,13 +5,15 @@ import customer from '../assert/customer.png'
 const CustomerSay = () => {
     return (
         <>
-            <TextCard heading={"Here's what our customer has to say"}/>
-            <div className="flex justify-center">
-                <img src={customer} className='rounded-full' alt="" />
-            </div>
-            <TextCard paragraph={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever"}/>       
+            <div className="space-y-0">
+                <TextCard heading={"Here's what our customer has to say"} />
+                <div className="flex justify-center">
+                    <img src={customer} className='rounded-full' alt="" />
+                </div>
+                <TextCard paragraph={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever"} />
 
-            <TextCard paragraph={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever"}/>
+                <TextCard paragraph={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever"} />
+            </div>
         </>
     );
 };
